461a2289b7465613909453093dc3b0daa0e0a828
Warren Harper <warrenjharper@gmail.com>
2016-08-01 09:59:29 -0400

    Add dockerfile-mode

--- emacs.d/.emacs.d/lisp/dot.el
+++ emacs.d/.emacs.d/lisp/dot.el
@@ -285,6 +285,10 @@
          (setq erc-fill-function 'erc-fill-static)
          (setq erc-fill-static-center 22)))
 
+(use-package dockerfile-mode
+  :ensure t
+  :mode "Dockerfile.*\\'")
+
 
 
 (provide 'dot)

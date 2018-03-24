79feb4a52545befdaf556b3c8721ab231914a379
Warren Harper <warrenjharper@gmail.com>
2018-03-24 09:15:00 -0400

    Install the latest org-mode

--- emacs.d/.emacs.d/init.el
+++ emacs.d/.emacs.d/init.el
@@ -71,7 +71,8 @@
 (require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                            ("marmalade" . "http://marmalade-repo.org/packages/")
-                           ("melpa" . "http://melpa.milkbox.net/packages/")))
+                           ("melpa" . "http://melpa.milkbox.net/packages/")
+                           ("org" . "http://orgmode.org/elpa/")))
 (package-initialize)
 
 (require 'dot)

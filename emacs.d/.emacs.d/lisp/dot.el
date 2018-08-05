305b37ece9e3a77c4ff96d4a5bfa47a85acbc33c
Warren Harper <warren@wjh.io>
2018-08-05 19:29:00 -0400

    Shorten autocompletion delay

--- emacs.d/.emacs.d/lisp/dot.el
+++ emacs.d/.emacs.d/lisp/dot.el
@@ -222,7 +222,7 @@
   :config (progn
             (require 'company)
             (setq company-tooltip-limit 20)                      ; bigger popup window
-            (setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
+            (setq company-idle-delay .1)                         ; decrease delay before autocompletion popup shows
             (setq company-echo-delay 0)                          ; remove annoying blinking
             (setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing))
             (add-hook 'after-init-hook 'global-company-mode) ; Use company mode everywhere

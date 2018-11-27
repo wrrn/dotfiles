13181da536adaeba817571a61ca566aea08359d0
Warren Harper <warren@wjh.io>
2018-11-27 08:59:40 -0500

    Remove marmalade as a package archive

--- emacs.d/.emacs.d/init.el
+++ emacs.d/.emacs.d/init.el
@@ -71,7 +71,6 @@
 ;;Package Setup
 (require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                           ("marmalade" . "http://marmalade-repo.org/packages/")
                            ("melpa" . "http://melpa.milkbox.net/packages/")
                            ("org" . "http://orgmode.org/elpa/")))
 (package-initialize)

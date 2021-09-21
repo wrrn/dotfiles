a3533f81324f636b57b57b89ccc1328cab7ca6a3
Warren Harper <warrenh@spotify.com>
2021-09-21 13:34:00 -0400

    Migrate to org-roam v2

--- emacs.d/.emacs.d/lisp/dot.el
+++ emacs.d/.emacs.d/lisp/dot.el
@@ -118,21 +118,18 @@
 
 (use-package org-roam
   :ensure t
-  :hook
-  (after-init . org-roam-mode)
   :custom
   (org-roam-directory (concat (getenv "HOME") "/.roam"))
   (org-roam-completion-system 'ivy)
-  :bind (:map org-roam-mode-map
-              (("C-c n l" . org-roam)
-               ("C-c n f" . org-roam-find-file)
-               ;; ("C-c n j" . org-roam-jump-to-index)
-               ("C-c n b" . org-roam-switch-to-buffer)
-               ("C-c n g" . org-roam-graph))
-              :map org-mode-map
-              (("C-c n i" . org-roam-insert)
-               ;; TODO: Use the variable here
-               ("C-j" . nil))))
+  (org-roam-v2-ack t)
+  
+  :bind  (("C-c n f" . org-roam-node-find)
+          ("C-c n b" . org-roam-switch-to-buffer)
+          ("C-c n g" . org-roam-graph)
+  :map org-mode-map
+  (("C-c n i" . org-roam-node-insert)
+   ;; TODO: Use the variable here
+   ("C-j" . nil))))
 
 ;; Deft helps me look up org-roam files quickly. Like rg or grep but using ivy on the fly
 (use-package deft

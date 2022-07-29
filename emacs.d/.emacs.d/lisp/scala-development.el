b5a26dffab9b7a7a03a1c53a9fc6d9c489c966de
Warren Harper <warrenh@spotify.com>
2022-07-29 10:10:21 -0400

    emacs - Fix sbt

--- emacs.d/.emacs.d/lisp/scala-development.el
+++ emacs.d/.emacs.d/lisp/scala-development.el
@@ -1,8 +1,10 @@
 ;;; scala-development.el  --- load all the scala development packages
 ;; Enable scala-mode for highlighting, indentation and motion commands
 
+;; Enable scala mode
 (use-package scala-mode
   :ensure t
+  :mode "\\.sc\\'"
   :interpreter
   ("scala" . scala-mode)
   :hook (scala-mode . lsp)
@@ -23,18 +25,23 @@
    'self-insert-command
    minibuffer-local-completion-map)
    ;; sbt-supershell kills sbt-mode:  https://github.com/hvesalai/emacs-sbt-mode/issues/152
-   (setq sbt:program-options '("-Dsbt.supershell=false"))
+  (setq sbt:program-options '("-Dsbt.supershell=false"))
+  :custom
+  (sbt:prefer-nested-projects t)
+  (sbt:program-name "/usr/local/bin/sbt")
 )
 
 ;; Add metals backend for lsp-mode
 (use-package lsp-metals
+  :ensure t
+  :custom
+  (lsp-metals-server-args '("-Dmetals.sbt-script=/usr/local/bin/sbt")))
+
+;; Posframe is a pop-up tool that must be manually installed for dap-mode
+(use-package posframe
   :ensure t)
 
-
-(use-package posframe
-  :ensure t
-  ;; Posframe is a pop-up tool that must be manually installed for dap-mode
-  )
+;; DAP is used for debugging code
 (use-package dap-mode
   :ensure t
   :hook

aa572acfd9371bcdfce2a86c6e2852caebf4572f
Warren Harper <wharper@pyroninc.com>
2024-09-18 10:38:38 -0400

    Add nix flake

--- flake.nix
+++ flake.nix
@@ -1,40 +1,58 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils/main";
   };
-  outputs = {self, nixpkgs, lib, ... }@inputs :{
-    packages.amethyst = lib.mkDerivation {
-      pname = "amethyst-conf";
-      version = "0.0.1";
-      src = ./amethyst;        
-    };
-    
-# xxxx    pakages.wezterm = mkDerivation {
-      
-#     };
 
-#     packages.emacs = mkDerivation {
+  #   outputs = {self, nixpkgs, flake-utils, ... }@inputs:
+  #     flake-utils.lib.eachSystem flake-utils.lib.allSystems (system: {hello = "name";});
+  # }
 
-#     };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
+    flake-utils.lib.eachSystem flake-utils.lib.allSystems (
+      system:
+      let
+        pkgs = import nixpkgs { system = "aarch64-darwin"; };
+        dotFiles = (
+          let
+            ignoredFiles = [
+              ".git"
+              ".gitignore"
+              "LICENSE"
+              "Makefile"
+              "flake.lock"
+              "flake.nix"
+            ];
+            isIgnored = f: builtins.elem f ignoredFiles;
+            allFiles = builtins.attrNames (builtins.readDir ./.);
+          in
+          builtins.filter (p: !isIgnored p) allFiles
+        );
 
-#     packages.fish = mkDerivation {
-      
-#     };
+        mkDotfile =
+          pname:
+          pkgs.stdenv.mkDerivation {
+            pname = "${pname}-conf";
+            version = "0.0.1";
+            src = ./${pname};
+            installPhase = ''
+              mkdir -p $out
+              cp -r $src/.* $out/ || true
+              cp -r $src/* $out/  || true
+            '';
+          };
 
-#     packages.git = mkDerivation {
-
-#     };
-
-#     packages.starship = mkDerivation {
-
-#     };
-
-#     packages.tridactyl = mkDerivation {
-      
-#     };
-
-#     packages.wezterm = mkDerivation {
-
-#     };            
-  };
+      in
+      {
+        packages = pkgs.lib.foldr (
+          dotfile: attrSet: attrSet // { "${dotfile}" = mkDotfile dotfile; }
+        ) { } dotFiles;
+      }
+    );
 }

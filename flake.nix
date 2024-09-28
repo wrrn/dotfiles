ce26214ba561cff8409b4ecfa7183c35e6f224a4
Warren Harper <warren@wjh.io>
2024-09-28 06:17:53 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -47,12 +47,9 @@
               cp -r $src/* $out/  || true
             '';
           };
-        packages = pkgs.lib.attrsets.genAttrs dotFiles mkDotfile;
-        defaults = {default = packages;};
-
       in
       {
-        packages = pkgs.lib.mergeAttrs packages defaults;
+        packages = pkgs.lib.attrsets.genAttrs dotFiles mkDotfile;
       }
     );
 }

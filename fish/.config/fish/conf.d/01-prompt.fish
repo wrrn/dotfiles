04c0c3c02724b67a4b1de578e9d60b0937e9037f
Warren Harper <warren@wjh.io>
2024-10-21 10:20:17 -0400

    Add output block

--- fish/.config/fish/conf.d/01-prompt.fish
+++ fish/.config/fish/conf.d/01-prompt.fish
@@ -1,3 +1,26 @@
+function humantime --argument-names ms --description "Turn milliseconds into a human-readable string"
+    #     Copyright © Jorge Bucaran <https://jorgebucaran.com>
+
+    # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    # The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+    # THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+    set --query ms[1] || return
+
+    set --local secs (math --scale=1 $ms/1000 % 60)
+    set --local mins (math --scale=0 $ms/60000 % 60)
+    set --local hours (math --scale=0 $ms/3600000)
+
+    test $hours -gt 0 && set --local --append out $hours"h"
+    test $mins -gt 0 && set --local --append out $mins"m"
+    test $secs -gt 0 && set --local --append out $secs"s"
+
+    set --query out && echo $out || echo $ms"ms"
+end
+
+
 if status is-interactive
     if type starship &>/dev/null
         function starship_transient_rprompt_func
@@ -6,6 +29,30 @@ if status is-interactive
         starship init fish | source
         enable_transience
         starship init fish | source
+
+
+        function write_box_start --on-event fish_preexec
+            # This add a number of spaces before the empty string and then
+            # replaces the spaces with ━.
+            printf '┗%*s┓\n\n' "$(math $(tput cols) - 3)" '' | sed 'y/ /━/'
+        end
+
+        function write_box_end --on-event fish_postexec
+            set box_prefix "┗"
+            set box_suffix "$(printf " %s ┛" "$(humantime $CMD_DURATION)")"
+
+
+            set box_prefix_length (string length "$box_prefix")
+            set box_suffix_length (string length "$box_suffix")
+
+            set fill_count (math "$(tput cols) - $box_prefix_length - $box_suffix_length - 1")
+
+            set box_fill "$(string repeat -n "$fill_count" "━")"
+
+
+            printf '\n%s%s%s\n\n' "$box_prefix" "$box_fill" "$box_suffix"
+
+        end
 
     end
 end

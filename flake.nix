2ce6497689d5052b5a9f932319d4bbd80aba991c
Warren Harper <warren@wjh.io>
2024-10-25 13:06:28 -0400

    Restructure

--- flake.nix
+++ flake.nix
@@ -4,10 +4,6 @@
     flake-utils.url = "github:numtide/flake-utils/main";
   };
 
-  #   outputs = {self, nixpkgs, flake-utils, ... }@inputs:
-  #     flake-utils.lib.eachSystem flake-utils.lib.allSystems (system: {hello = "name";});
-  # }
-
   outputs =
     {
       self,
@@ -15,41 +11,46 @@
       flake-utils,
       ...
     }@inputs:
-    flake-utils.lib.eachSystem flake-utils.lib.allSystems (
-      system:
-      let
-        pkgs = import nixpkgs { system = system; };
-        dotFiles = (
-          let
-            ignoredFiles = [
-              ".git"
-              ".gitignore"
-              "LICENSE"
-              "Makefile"
-              "flake.lock"
-              "flake.nix"
-            ];
-            isIgnored = f: builtins.elem f ignoredFiles;
-            allFiles = builtins.attrNames (builtins.readDir ./.);
-          in
-          builtins.filter (p: !isIgnored p) allFiles
-        );
+    let
+      dotFiles = (
+        let
+          ignoredFiles = [
+            ".git"
+            ".gitignore"
+            "LICENSE"
+            "Makefile"
+            "flake.lock"
+            "flake.nix"
+          ];
+          isIgnored = f: builtins.elem f ignoredFiles;
+          allFiles = builtins.attrNames (builtins.readDir ./.);
+        in
+        builtins.filter (p: !isIgnored p) allFiles
+      );
 
-        mkDotfile =
-          pname:
-          pkgs.stdenv.mkDerivation {
-            inherit pname;
-            name = pname;
-            src = ./${pname};
-            installPhase = ''
-              mkdir -p $out
-              cp -r $src/.* $out/ || true
-              cp -r $src/* $out/  || true
-            '';
-          };
-      in
-      {
-        packages = pkgs.lib.attrsets.genAttrs dotFiles mkDotfile;
-      }
-    );
+      mkDotfile =
+        { pkgs, pname }:
+        pkgs.stdenv.mkDerivation {
+          inherit pname;
+          name = pname;
+          src = ./${pname};
+          installPhase = ''
+            mkdir -p $out
+            cp -r $src/.* $out/ || true
+            cp -r $src/* $out/  || true
+          '';
+        };
+
+      packageDotfiles =
+        { pkgs }: pkgs.lib.attrsets.genAttrs dotFiles (dir: (pkgs.callPackage mkDotfile { pname = dir; }));
+    in
+    {
+      packages = flake-utils.lib.eachSystem flake-utils.lib.allSystems (
+        system:
+        let
+          pkgs = import nixpkgs { system = system; };
+        in
+        pkgs.callPackage packageDotfiles { }
+      );
+    };
 }

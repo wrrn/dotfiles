9a09a57684aa10222c31682dc23840f10115d0c1
Warren Harper <warren@wjh.io>
2024-12-09 11:29:06 -0500

    emacs: themes to own file

--- emacs/.config/emacs/lisp/ui-config.el
+++ emacs/.config/emacs/lisp/ui-config.el
@@ -1,95 +1,5 @@
 ;; ui-config.el --- Configure the ui-components
-
-(use-package autothemer
-  :ensure t)
-
-(use-package minimal-theme
-  ;; :ensure t
-  ;; :config (load-theme 'minimal-light t)
-  )
-
-(use-package almost-mono-themes
-  :ensure t
-  ;; :config
-  ;; (load-theme 'almost-mono-black t)
-  ;; (load-theme 'almost-mono-gray t)
-  ;; (load-theme 'almost-mono-cream t)
-  ;; (load-theme 'almost-mono-white t)
-  )
-
-(use-package catppuccin-theme
-  :ensure t
-  ;; :init
-  ;; (load-theme 'catppuccin :no-confirm)
-  ;; (setq catppuccin-flavor 'frappe) ;; or 'frappe, 'latte, 'macchiato, or 'mocha
-  ;; (catppuccin-reload)
-  )
-
-(use-package rose-pine-color-theme
-  :ensure t
-  :straight (rose-pine-emacs
-             :host github
-             :repo "thongpv87/rose-pine-emacs"
-             :branch "master")
-  :requires autothemer
-  :init (load-theme 'rose-pine-dawn t))
-
-
-(use-package kaolin-themes
-  :ensure t
-  ;;   :config
-  ;;   (load-theme 'kaolin-valley-light :no-confirm)
-  ;;   (kaolin-treemacs-theme)
-  )
-
-(use-package sketch-themes
-  :ensure t
-  :config
-  ;; Load black version
-  ;; (load-theme 'sketch-black t)
-  ;; Load white version
-  (load-theme 'sketch-white t)
-  )
-
-(use-package subatomic-theme
-  :ensure t
-  ;; :config (load-theme 'subatomic)
-  )
-
-(use-package darktooth-theme
-  :ensure t
-  ;;   :config (load-theme 'darktooth)
-  )
-
-(use-package soothe-theme
-  :ensure t
-  ;; :config (load-theme 'soothe)
-  )
-
-(use-package jazz-theme
-  :ensure t
-  ;;   :config (load-theme 'jazz)
-  )
-
-(use-package goose-theme
-  :ensure t)
-
-(use-package gruvbox-theme
-  :ensure t
-  ;; :config (load-theme 'gruvbox-light-soft)
-  )
-
-(use-package kanagawa-theme
-  :ensure t
-  :straight (kanagawa-theme :type git :host github :repo  "jasonm23/emacs-theme-kanagawa")
-  :requires autothemer
-  ;; :config (load-theme 'kanagawa)
-  )
-
-(use-package modus-themes
-  :ensure t
-  ;; :config (load-theme 'modus-vivendi-tinted)
-  )
+(require 'theme "./ui/theme.el")
 
 (use-package simple
   :straight f
@@ -98,14 +8,14 @@
   :init (setq visual-line-mode 80)
   :config (global-visual-line-mode))
 
-(use-package auto-dim-other-buffers
-  :ensure t
-  :custom
-  (auto-dim-other-buffers-dim-on-focus-out nil)
-  :custom-face
-  (auto-dim-other-buffers-face ((t (:background "#ddd"))))
-  :config
-  (auto-dim-other-buffers-mode t))
+;; (use-package auto-dim-other-buffers
+;;   :ensure t
+;;   :custom
+;;   (auto-dim-other-buffers-dim-on-focus-out nil)
+;;   :custom-face
+;;   (auto-dim-other-buffers-face ((t (:background "#ddd"))))
+;;   :config
+;;   (auto-dim-other-buffers-mode nil))
 
 ;; Make a small center margin
 ;; (use-package fringe
---
+++ emacs/.config/emacs/lisp/ui/theme.el
@@ -0,0 +1,95 @@
+;; theme.el -- Configure the them we want to use
+
+(use-package autothemer
+  :ensure t)
+
+(use-package minimal-theme
+  ;; :ensure t
+  ;; :config (load-theme 'minimal-light t)
+  )
+
+(use-package almost-mono-themes
+  :ensure t
+  ;; :config
+  ;; (load-theme 'almost-mono-black t)
+  ;; (load-theme 'almost-mono-gray t)
+  ;; (load-theme 'almost-mono-cream t)
+  ;; (load-theme 'almost-mono-white t)
+  )
+
+(use-package catppuccin-theme
+  :ensure t
+  ;; :init
+  ;; (load-theme 'catppuccin :no-confirm)
+  ;; (setq catppuccin-flavor 'frappe) ;; or 'frappe, 'latte, 'macchiato, or 'mocha
+  ;; (catppuccin-reload)
+  )
+
+(use-package rose-pine-color-theme
+  :ensure t
+  :straight (rose-pine-emacs
+             :host github
+             :repo "thongpv87/rose-pine-emacs"
+             :branch "master")
+  :requires autothemer
+  ;; :init (load-theme 'rose-pine-moon t)
+  )
+
+
+(use-package kaolin-themes
+  :ensure t
+  ;;   :config
+  ;;   (load-theme 'kaolin-valley-light :no-confirm)
+  ;;   (kaolin-treemacs-theme)
+  )
+
+(use-package sketch-themes
+  :ensure t
+  :config
+  ;; Load black version
+  ;; (load-theme 'sketch-black t)
+  ;; Load white version
+  (load-theme 'sketch-white t)
+  )
+
+(use-package subatomic-theme
+  :ensure t
+  ;; :config (load-theme 'subatomic)
+  )
+
+(use-package darktooth-theme
+  :ensure t
+  ;;   :config (load-theme 'darktooth)
+  )
+
+(use-package soothe-theme
+  :ensure t
+  ;; :config (load-theme 'soothe)
+  )
+
+(use-package jazz-theme
+  :ensure t
+  ;;   :config (load-theme 'jazz)
+  )
+
+(use-package goose-theme
+  :ensure t)
+
+(use-package gruvbox-theme
+  :ensure t
+  ;; :config (load-theme 'gruvbox-light-soft)
+  )
+
+(use-package kanagawa-theme
+  :ensure t
+  :straight (kanagawa-theme :type git :host github :repo  "jasonm23/emacs-theme-kanagawa")
+  :requires autothemer
+  :config (load-theme 'kanagawa)
+  :custom-face
+  (fringe ((t (:background "#181820")))))
+
+  (use-package modus-themes
+    :ensure t
+    ;; :config (load-theme 'modus-vivendi-tinted)
+    )
+  (provide 'theme)

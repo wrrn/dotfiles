a1cad70b9251f6db203e2c11d7db20d9b57d8b66
Warren Harper <warren@wjh.io>
2025-03-18 13:14:17 -0400

    Migrate from asdf to mise

--- fish/.config/fish/conf.d/05-asdf-java.fish
+++
@@ -1,2 +0,0 @@
-set asdf_java_home_script ~/.asdf/plugins/java/set-java-home.fish
-[ -f $asdf_java_setup_script ] && source $asdf_java_setup_script
--- fish/.config/fish/conf.d/99-asdf.fish
+++
@@ -1,3 +0,0 @@
-set ASDF_DIR (asdf info | grep ASDF_DIR | string replace "ASDF_DIR=" "")
-
-source $ASDF_DIR/asdf.fish

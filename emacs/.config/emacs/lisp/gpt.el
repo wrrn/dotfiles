c531b625eea3d98d2fdd1768d8025fb5724d49f6
Warren Harper <warren@wjh.io>
2025-03-27 13:29:26 -0400

    feat: Update gptel to use anthropic

--- emacs/.config/emacs/lisp/gpt.el
+++ emacs/.config/emacs/lisp/gpt.el
@@ -1,5 +1,11 @@
 ;;; gpt.el  --- configure gptel
 (use-package gptel
-  :ensure t)
+  :ensure t
+  :custom
+  (gptel-model 'claude-3-7-sonnet-20250219)
+  :config
+  (gptel-make-anthropic "Claude" :stream t :key #'gptel-api-key))
+
+
 
 (provide 'gpt)

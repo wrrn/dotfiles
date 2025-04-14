1e61e4792582c3a0e2cf56167d55d889d8855fee
Warren Harper <warren@wjh.io>
2025-04-14 11:35:13 -0400

    feat: Use gemini for gptel

--- emacs/.config/emacs/lisp/gpt.el
+++ emacs/.config/emacs/lisp/gpt.el
@@ -2,9 +2,10 @@
 (use-package gptel
   :ensure t
   :custom
-  (gptel-model 'claude-3-7-sonnet-20250219)
+  (gptel-model 'gemini-2.5-pro-preview-03-25)
   :config
-  (gptel-make-anthropic "Claude" :stream t :key #'gptel-api-key))
+  (gptel-make-anthropic "Claude" :stream t :key #'gptel-api-key)
+  (setq gptel-backend (gptel-make-gemini "Gemini" :stream t :key #'gptel-api-key)))
 
 
 

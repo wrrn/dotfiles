253a066c0d1de0bb514672411d90ba0c43fbefd8
Warren Harper <warren@wjh.io>
2025-06-05 19:35:31 -0400

    feat: Use claude-opus-4

--- emacs/.config/emacs/lisp/gpt.el
+++ emacs/.config/emacs/lisp/gpt.el
@@ -2,15 +2,16 @@
 (use-package gptel
   :ensure t
   :custom
-  (gptel-model 'gemini-2.5-pro-preview-03-25)
+  (gptel-model 'claude-opus-4-20250514)
   :bind (
          ("C-x C-M-a" . gptel-add)
          ("C-x C-M-m" . gptel-menu)
          ("C-x C-M-c" . gptel)
          )
   :config
-  (gptel-make-anthropic "Claude" :stream t :key #'gptel-api-key)
-  (setq gptel-backend (gptel-make-gemini "Gemini" :stream t :key #'gptel-api-key)))
+  (gptel-make-gemini "Gemini" :stream t :key #'gptel-api-key)
+  (setq gptel-backend (gptel-make-anthropic "Claude" :stream t :key #'gptel-api-key)))
+
 
 
 

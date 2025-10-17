80339838fd2f9ac022dcb9126a712a0c871d33c7
Warren Harper <warren@harperwarren.com>
2025-10-17 12:58:23 -0400

    sketchbar - make sketchybar worth with amethyst

--- sketchybar/.config/sketchybar/bar.lua
+++ sketchybar/.config/sketchybar/bar.lua
@@ -2,9 +2,10 @@ local colors = require("colors")
 
 -- Equivalent to the --bar domain
 sbar.bar({
-  height = 40,
-  color = colors.bar.bg,
-  blur_radius = 30,
-  padding_right = 2,
-  padding_left = 2,
+    height = 40,
+    notch_display_height = 20,
+    color = colors.bar.bg,
+    blur_radius = 30,
+    padding_right = 2,
+    padding_left = 2,
 })

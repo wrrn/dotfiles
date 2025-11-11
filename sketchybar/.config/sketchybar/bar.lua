10432feae2e94a31d326ba3e4800b41c0916c945
Warren Harper <warren@harperwarren.com>
2025-11-11 12:43:30 -0500

    sketchbar - Fix the height issues

--- sketchybar/.config/sketchybar/bar.lua
+++ sketchybar/.config/sketchybar/bar.lua
@@ -2,8 +2,9 @@ local colors = require("colors")
 
 -- Equivalent to the --bar domain
 sbar.bar({
-    height = 40,
+    height = 20,
     notch_display_height = 20,
+    topmost = true,
     color = colors.bar.bg,
     blur_radius = 30,
     padding_right = 2,

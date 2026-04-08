6f0fc51969b2caf95d65a4b20c64645e802af20a
Warren Harper <warren@wrrn.org>
2026-04-08 12:00:16 -0400

    fish: add nmtui theme

---
+++ fish/.config/fish/conf.d/newt.fish
@@ -0,0 +1,16 @@
+set -Ux NEWT_COLORS '\
+    root=black,white \
+    window=black,white \
+    border=black,white \
+    title=black,white \
+    label=black,white \
+    listbox=black,white \
+    actlistbox=white,black \
+    actsellistbox=white,black \
+    entry=black,white \
+    textbox=black,white \
+    button=white,black \
+    actbutton=white,red \
+    checkbox=black,white \
+    actcheckbox=white,black \
+    compactbutton=black,white'

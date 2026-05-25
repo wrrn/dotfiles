3e07f57e9858db1d4f0a854802044592de22972a
Warren Harper <me@wrrn.org>
2026-05-25 15:17:16 -0400

    Add emacsclient stuff for emacs

--- emacs/dot-config/emacs/lisp/term-config.el
+++ emacs/dot-config/emacs/lisp/term-config.el
@@ -1,5 +1,16 @@
 ;; term-config.el --- Configure Terminal specific packages
 
+(use-package with-editor
+  :ensure t)
+
+;; Helper function for ghostel-pre-spawn-hook
+;; Uses with-editor's internal setup to configure EDITOR in spawned shells
+(defun ghostel-with-editor-setup ()
+  "Set up EDITOR environment variable for ghostel terminal.
+  Calls the internal with-editor setup function to configure
+  emacsclient as the EDITOR for git commits and other editor-opening commands."
+  (with-editor--setup))
+
 (use-package ghostel
   :ensure t
   :custom
@@ -8,6 +19,7 @@
   ;; forward the key to the shell — so meow's j/k/h/l/n would drop us out
   ;; of Emacs mode and cycle shell history instead of navigating the buffer.
   (ghostel-readonly-fast-exit nil)
+  :hook (ghostel-pre-spawn . ghostel-with-editor-setup)
   :bind (("C-x p t" . ghostel-project)
          :map ghostel-semi-char-mode-map
          ("C-c r" . rename-buffer)
--- fish/dot-config/fish/conf.d/05-emacs.fish
+++ fish/dot-config/fish/conf.d/05-emacs.fish
@@ -1,13 +1,17 @@
-set -x EMACS_VTERM_PATH ~/.config/emacs/straight/build/vterm
+# set -x EMACS_VTERM_PATH ~/.config/emacs/straight/build/vterm
 
-if [ "$INSIDE_EMACS" = vterm ] && [ -n $EMACS_VTERM_PATH ] && [ -f {$EMACS_VTERM_PATH}/etc/emacs-vterm.fish ]
-    source {$EMACS_VTERM_PATH}/etc/emacs-vterm.fish
-end
+# if [ "$INSIDE_EMACS" = vterm ] && [ -n $EMACS_VTERM_PATH ] && [ -f {$EMACS_VTERM_PATH}/etc/emacs-vterm.fish ]
+#     source {$EMACS_VTERM_PATH}/etc/emacs-vterm.fish
+# end
 
-if [ "$INSIDE_EMACS" = vterm ] || string match -q -- "*eat" "$INSIDE_EMACS"
-   set -x EDITOR emacsclient
-else if string match -q -- '*kitty' $TERM
-    set -x EDITOR {$HOME}/.local/bin/kitty.emacs
-else
+# if [ "$INSIDE_EMACS" = vterm ] || string match -q -- "*eat" "$INSIDE_EMACS"
+#    set -x EDITOR emacsclient
+# else if string match -q -- '*kitty' $TERM
+#     set -x EDITOR {$HOME}/.local/bin/kitty.emacs
+# else
+#     set -x EDITOR 'emacsclient -nw'
+# end
+
+if [ "$INSIDE_EMACS" != ghostel ]
     set -x EDITOR 'emacsclient -nw'
 end

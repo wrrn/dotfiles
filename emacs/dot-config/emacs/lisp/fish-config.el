9804816155ce1a8aa257d8ae72979d85db7ff531
Warren Harper <me@wrrn.org>
2026-06-09 15:56:22 -0400

    emacs: fish: add new capf for fish mode

--- emacs/dot-config/emacs/lisp/fish-config.el
+++ emacs/dot-config/emacs/lisp/fish-config.el
@@ -1,6 +1,35 @@
 ;;; fish-config.el  --- configure meow mode
 ;; fish configuration
+
+;; 
+(defun wrrn/fish-capf ()
+  (let* ((line (buffer-substring-no-properties
+                (line-beginning-position) (point)))
+         (start (save-excursion
+                  (skip-syntax-backward "^ " (line-beginning-position))
+                  (point)))
+         (pairs (with-temp-buffer
+                  (call-process "fish" nil t nil "-c"
+                                (concat "complete -C"
+                                        (shell-quote-argument line)))
+                  (mapcar (lambda (l) (split-string l "\t"))
+                          (split-string (buffer-string) "\n" t))))
+         (cands (mapcar #'car pairs))
+         (descs (mapcar (lambda (p) (cadr p)) pairs)))
+    (when cands
+      (list start (point) cands
+            :annotation-function
+            (lambda (cand)
+              (when-let ((i (cl-position cand cands :test #'string=)))
+                (when-let ((d (nth i descs)))
+                  (concat "  " d))))))))
+
+
 (use-package fish-mode
-  :ensure t)
+  :ensure t
+  :hook ((fish-mode . (lambda ()
+                        (add-hook 'completion-at-point-functions
+                                  #'wrrn/fish-capf nil t)))))
+
 
 (provide 'fish-config)

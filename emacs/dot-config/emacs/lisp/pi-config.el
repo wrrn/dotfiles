f8184e4c3d44761ec30518ba85c96adaf31439cd
Warren Harper <me@wrrn.org>
2026-09-09 19:49:50 -0400

    emacs: pilish: use pilish for the toggle command

--- emacs/dot-config/emacs/lisp/pi-config.el
+++ emacs/dot-config/emacs/lisp/pi-config.el
@@ -1,9 +1,9 @@
-;;; pi-config.el  --- configure pi-coding-agent
+;;; pi-config.el  --- configure pi-coding-agent  -*- lexical-binding: t; -*-
 ;; Configuration
 
 (use-package pilish
   :ensure t
-  :bind ("C-c p" . pilish-toggle)
+  :bind ("C-c p" . pilish)
   )
 
 (provide 'pi-config)

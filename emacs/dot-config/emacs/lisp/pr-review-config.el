9990d741d4a77f416bc04d6a237f1d748a798f57
Warren Harper <me@wrrn.org>
2026-09-21 15:00:42 -0400

    emacs: pr-review: fix predefined-queries

--- emacs/dot-config/emacs/lisp/pr-review-config.el
+++ emacs/dot-config/emacs/lisp/pr-review-config.el
@@ -3,13 +3,14 @@
 
 (use-package pr-review
   :ensure t
-  :custom
-  ((pr-review-search-predefined-queries . '(
-                                            ("is:pr state:open archived:false sort:updated-desc org:xonasystems" . "Xona")
-                                            ("is:pr archived:false author:@me is:open" . "Created")
-                                            ("is:pr archived:false assignee:@me is:open" . "Assigned")
-                                            ("is:pr archived:false mentions:@me is:open" . "Mentioned")
-                                            ("is:pr archived:false review-requested:@me is:open" . "Review requests"))
-                                        )))
+  :custom (
+           (pr-review-search-predefined-queries '(
+                                                  ("is:pr state:open archived:false -reviewed-by:@me org:xonasystems" . "Xona - Unreviewed")
+                                                  ("is:pr state:open archived:false sort:updated-desc org:xonasystems" . "Xona")
+                                                  ("is:pr archived:false author:@me is:open" . "Created")
+                                                  ("is:pr archived:false assignee:@me is:open" . "Assigned")
+                                                  ("is:pr archived:false mentions:@me is:open" . "Mentioned")
+                                                  ("is:pr archived:false review-requested:@me is:open" . "Review requests"))
+                                                )))
 
 (provide 'pr-review-config)
